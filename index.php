b4090f9d607afbc1358b62f3f81f8f97c9ae7fba
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-11-18 07:52:00 +0900

    [2024-11-18

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="ko">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>우당탕탕 php 공부</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div id="wrap">
+        <header id="header">
+            <h1>성준이의 우당탕탕 php 공부</h1>
+        </header>
+
+        <section class="section">
+            <div class="contents">
+                <h2>전설적인 PHP 공부 내용</h2>
+                <div class="table">
+                    <div>
+                        <table class="table_line">
+                            <tr>
+                                <th>1일차</th>
+                                <td><a href="day-1/today.php">년도 확인하기</a></td>
+                            </tr>
+                        </table>
+                    </div>
+
+                    <!-- <div>
+                        <table class="table_line">
+                            <tr>
+                                <th>1일차</th>
+                                <td><a href="">년도 확인하기</a></td>
+                            </tr>
+                        </table>
+                    </div> -->
+                </div>
+            </div>
+        </section>
+
+        <footer id="footer">
+            <div class="footer-title">
+                <h3>사이트 개발자 : 장성준</h3>
+            </div>
+        </footer>
+    </div>
+</body>
+
+</html>
