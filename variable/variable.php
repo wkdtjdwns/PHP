b81befce4ae7c0482a52a0a1dd0e09b4ce499497
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-12-11 08:33:37 +0900

    [2024-12-11]

---
+++ variable/variable.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>변수</title>
+    <style>
+        a {
+            color: blue;
+            text-decoration: none;
+        }
+
+        .table {
+            width: 100%;
+            justify-items: center;
+            padding: 20px 0px;
+        }
+
+        .table_line {
+            padding: 10px 20px;
+            border-collapse: collapse;
+        }
+
+        .table_line th,
+        td {
+            border: 1px solid black;
+        }
+
+        .table_line td {
+            padding: 10px 20px;
+        }
+    </style>
+</head>
+<body>
+    <table class="table_line">
+        <tr>
+            <div>
+                <td><a href="./variableTest.php">1. 변수</a></td>
+                <td><a href="./local_global.php">2. 지역-전역 변수</a></td>
+            </div>
+        </tr>
+    </table>
+</body>
+</html>
