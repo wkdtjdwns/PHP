bfe8846852ace9903c9eea7616872bc6ecf202c8
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-12-11 08:35:09 +0900

    [2024-12-11]

---
+++ dataType/array.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Array</title>
+</head>
+<body>
+    <?php
+        $a = array("가", "나", "다", "라");
+
+        echo $a[0] ."<br>";
+        echo $a[1] ."<br>";
+        echo $a[2] ."<br>";
+        echo $a[3] ."<br>";
+    ?>
+
+    <hr>
+    <h1>소스 코드</h1>
+
+    $a = array("가", "나", "다", "라");
+
+    echo $a[0] ."&lt;br&gt;";<br>
+    echo $a[1] ."&lt;br&gt;";<br>
+    echo $a[2] ."&lt;br&gt;";<br>
+    echo $a[3] ."&lt;br&gt;";<br>
+</body>
+</html>
