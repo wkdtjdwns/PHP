a717e8d1f62df08897a5f55666db8e27b4540320
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-12-16 08:19:08 +0900

    [2024-12-16]

---
+++ operator/operator.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>연산자</title>
+    <style>
+        a {
+            color: blue;
+            text-decoration: none;
+        }
+
+        .table {
+            width: 100%;
+            justify-items: center;
+            padding: 20px 0px;
+        }
+
+        .table_line {
+            padding: 10px 20px;
+            border-collapse: collapse;
+        }
+
+        .table_line th,
+        td {
+            border: 1px solid black;
+        }
+
+        .table_line td {
+            padding: 10px 20px;
+        }
+    </style>
+</head>
+<body>
+    <table class="table_line">
+        <tr>
+            <div>
+                <td><a href="./operator_1.php">1. 산술, 증감 연산자</a></td>
+                <td><a href="./operator_2.php">2. 비교, 대입 연산자</a></td>
+                <td><a href="./operator_3.php">3. 논리 연산자</a></td>
+                <td><a href="./operator_4.php">4. 삼항 연산자</a></td>
+            </div>
+        </tr>
+    </table>
+</body>
+</html>
