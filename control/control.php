c03309478145e32f9a8cf26541e142fa2a9b3300
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-12-17 08:38:02 +0900

    [2024-12-17]

--- control/control.php
+++ control/control.php
@@ -39,6 +39,15 @@
                 <td><a href="./switch.php">2. [조건문] switch문</a></td>
                 <td><a href="./for.php">3. [반복문] for문</a></td>
                 <td><a href="./foreach.php">4. [반복문] foreach문</a></td>
+                <td><a href="./while.php">5. [반복문] while문</a></td>
+            </div>
+        </tr>
+
+        <tr>
+            <div>
+                <td><a href="./do_while.php">6. [반복문] do-while문</a></td>
+                <td><a href="./break.php">7. [기타 제어문] break</a></td>
+                <td><a href="./continue.php">8. [기타 제어문] continue</a></td>
             </div>
         </tr>
     </table>
