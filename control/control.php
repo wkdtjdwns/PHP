9c2843e4c3c135a8a2e822d167289e730ca50c82
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-12-17 08:29:41 +0900

    [2024-12-17]

---
+++ control/control.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>제어문</title>
+    <style>
+        a {
+            color: blue;
+            text-decoration: none;
+        }
+
+        .table {
+            width: 100%;
+            justify-items: center;
+            padding: 20px 0px;
+        }
+
+        .table_line {
+            padding: 10px 20px;
+            border-collapse: collapse;
+        }
+
+        .table_line th,
+        td {
+            border: 1px solid black;
+        }
+
+        .table_line td {
+            padding: 10px 20px;
+        }
+    </style>
+</head>
+<body>
+    <table class="table_line">
+        <tr>
+            <div>
+                <td><a href="./if.php">1. [조건문] if문</a></td>
+                <td><a href="./switch.php">2. [조건문] switch문</a></td>
+                <td><a href="./for.php">3. [반복문] for문</a></td>
+                <td><a href="./foreach.php">4. [반복문] foreach문</a></td>
+            </div>
+        </tr>
+    </table>
+</body>
+</html>
