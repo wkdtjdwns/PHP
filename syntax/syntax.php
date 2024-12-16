4572dc764f14e7b0fce5c37057aaae44e295c56c
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-12-17 08:00:20 +0900

    [2024-12-17]

--- syntax/syntax.php
+++ syntax/syntax.php
@@ -3,19 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>php 기초 문법</title>
+    <title>기본 문법</title>
 </head>
 <body>
     <?php // php 구문 시작
 
         class Grammer // 클래스 정의
         {
-            public $name = "Welcome PHP!" // "Welcome PHP!"를 변수 name에 할당
+            public $name = "Welcome PHP!"; // "Welcome PHP!"를 변수 name에 할당
             
-            public function phpStudy($year); // 메소드 정의
+            public function phpStudy($year) // 메소드 정의
             {
-                echo "변수 name은 {$this->name}입니다."<br/>
-                echo "변수 year {$year}입니다."<br/>;
+                echo "변수 name은 {$this->name}입니다.<br/>";
+                echo "변수 year {$year}입니다.<br/>";
                 echo $this->name . $year ."<br/>";
             }
         }
