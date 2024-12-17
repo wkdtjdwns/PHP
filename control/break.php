bd6d79f16679901efd2941619902f1c79dbd7c8c
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-12-18 07:45:29 +0900

    [2024-12-18]

---
+++ control/break.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>break</title>
+</head>
+<body>
+    <?php
+        for ($i = 1; $i <= 10; $i++)
+        {
+            echo "{$i}<br>";
+            if ($i === 5)
+            {
+                break;
+            }
+        }
+    ?>
+
+    <hr>
+    <h1>소스 코드</h1>
+
+        for ($i = 1; $i <= 10; $i++)<br>
+        {<br>
+            echo "{$i}&lt;br&gt;";<br>
+            if ($i === 5)<br>
+            {<br>
+                break;<br>
+            }<br>
+        }
+</body>
+</html>
