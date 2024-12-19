1d0a6f8a3c702a63921a02e7efbb17935167dc1f
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-12-20 08:36:56 +0900

    [2024-12-20]

---
+++ class/protected.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>상속</title>
+</head>
+<body>
+    <?php
+        class OutSider
+        {
+            protected $s = "OutSider";
+        }
+        
+        class OutSider2 extends OutSider
+        {
+            public function echoS()
+            {
+                echo $this->s;
+            }
+        }
+
+        $outSider = new OutSider2();
+        $outSider->echoS();
+    ?>
+
+    <hr>
+    <h1>소스 코드</h1>
+
+        class OutSider<br>
+        {<br>
+            protected $s = "OutSider";<br>
+        }<br><br>
+        
+        class OutSider2 extends OutSider<br>
+        {<br>
+            public function echoS()<br>
+            {<br>
+                echo $this->s;<br>
+            }<br>
+        }<br><br>
+
+        $outSider = new OutSider2();<br>
+        $outSider->echoS();
+</body>
+</html>
