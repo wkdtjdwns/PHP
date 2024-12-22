8c013e8eaf0787e02703f28ac57e56b6f7ffb911
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-12-23 08:30:37 +0900

    [2024-12-23]

--- class/protected.php
+++ class/protected.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>상속</title>
+    <title>protected</title>
 </head>
 <body>
     <?php
